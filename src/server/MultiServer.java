3bbdb466b1b428bc1197667565835267aad87a97
paprika <caterina-lafirenze@virgilio.it>
2025-10-21 09:03:20 +0200

    eddai dannato

--- src/server/MultiServer.java
+++ src/server/MultiServer.java
@@ -7,27 +7,27 @@ import java.net.*;
 public class MultiServer extends ServerSocket {
     private int PORT = 8080;
 
-    MultiServer( int port)throws IOException{
+    MultiServer(int port)throws IOException{
         this.PORT = port;
         run();
     }
 
     void run()throws IOException {
+
         ServerSocket s = new ServerSocket(PORT);
-        Socket socket = s.accept();
-        System.out.println( "Connessione iniziata: " + socket.getInetAddress());
-        
+        System.out.println( "Connessione iniziata: ");
         try{
             while(true){
+                Socket socket = s.accept();
                 try{
                     new ServerOneClient(socket);
                 }catch(IOException e){
                     socket.close();
-                } 
+                }
             }
         } finally{
             s.close();
-            System.out.println( "Connessione chiusa: " + socket.getInetAddress());
+            System.out.println( "Connessione chiusa: ");
         }
     }
 
@@ -36,6 +36,7 @@ public class MultiServer extends ServerSocket {
         
         try {
             MultiServer ms = new MultiServer(8080);
+            //ms.run();
         }catch(IOException e){
             e.printStackTrace();
         }
--- src/server/ServerOneClient.java
+++ src/server/ServerOneClient.java
@@ -7,28 +7,33 @@ import java.io.*;
 
 public class ServerOneClient extends Thread {
     private Socket socket;
-    private BufferedReader in;
-    private PrintWriter out;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
     private QTMiner kmeans;
 
     ServerOneClient(Socket s) throws IOException {
         this.socket = s;
-        in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
-        out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+        out =new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
         start();
     }
 
     public void run(){
         try {
             while (true) {
-                String str = in.readLine();
-                if (str.equals("END")) break;
-                System.out.println("Echoing: " + str);
-                out.println(str);
-                //System.out.println();
+                
+                Object o =in.readObject();
+                //Integer str = (Integer) o;
+                if (o.equals("END")) break;
+                System.out.println("Echoing: " + o);
+                out.writeObject("Echoing: " + o);
+                out.flush();
+
+                //out.writeChars(str);
+                //out.println();
             }
             System.out.println("closing...");
-        } catch(IOException e) {
+        } catch(IOException | ClassNotFoundException e) {
             System.err.println("IO Exception");
         } finally {
             try {
